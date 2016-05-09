e99f5cd7cda37be71bf97ab3ffd7f66ecb05bdfc
toadette77 <toadette007@outlook.de>
2016-05-09 10:05:42 +0200

    moved files, changed directory and name

--- CNC/Interface/design/toggle.js
+++
@@ -1,7 +0,0 @@
-function toggleButton(TID){
-	if (document.getElementById(TID).innerHTML == "Start"){
-		(document.getElementById(TID).innerHTML = "Stop");
-	}else{
-		(document.getElementById(TID).innerHTML = "Start");
-	}
-};
