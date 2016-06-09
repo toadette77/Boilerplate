14809f5d5452e6aaf51b5df1f97881879f918aff
Cookie Engineer <cookiengineer@protonmail.ch>
2016-06-09 16:21:10 +0200

    :rocket: Some Fixes for table layout

--- CNC/Interface/source/index.js
+++ CNC/Interface/source/index.js
@@ -254,7 +254,7 @@ if (menu !== null) {
 	var submit = section.querySelector('div.action-form button');
 	if (submit !== null) {
 
-		button.onclick = function() {
+		submit.onclick = function() {
 
 			var type  = section.querySelector('div.action-form select');
 			var input = section.querySelector('div.action-form input');
